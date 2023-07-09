ad48559d524047d38acd5f9cf36a2865cac0acde
Junsung Park / Daanta <daanta@naver.com>
2023-07-09 23:56:21 +0900

    230709_2356 Studying scrapping Noeul

--- src/test/java/CampIfnoes/D230702_01_Noel_DateSeqCaculation.java
+++ src/test/java/CampIfnoes/D230702_01_Noel_DateSeqCaculation.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 @Slf4j
 public class D230702_01_Noel_DateSeqCaculation {
@@ -11,36 +12,32 @@ public class D230702_01_Noel_DateSeqCaculation {
     public int calc(String orgDateStr, String trgDateStr, String orgSeqStr) {
 
         LocalDate orgDate = LocalDate.parse(orgDateStr, java.time.format.DateTimeFormatter.BASIC_ISO_DATE);
-        log.debug("DATE: {}", orgDate);
+        log.debug("ORGDATE LocalDate: {}", orgDate);
+
+        LocalDate trgDate = LocalDate.parse(trgDateStr, java.time.format.DateTimeFormatter.BASIC_ISO_DATE);
+        log.debug("TRGDATE LocalDate: {}", trgDate);
+
+        long dateDiff = ChronoUnit.DAYS.between(orgDate, trgDate);
+        log.debug("DATE DIFF RESULT OF TRG - ORG = {} - {} = {}", trgDate, orgDate, dateDiff);
 
         int orgSeq = Integer.parseInt(orgSeqStr);
-        LocalDate resultDate = orgDate.plusDays(orgSeq);
-        int year = resultDate.getYear();
-        int month = resultDate.getMonthValue();
-        int day = resultDate.getDayOfMonth();
-        log.debug("[ORG] Y:{} / M: {} / D: {}", year, month, day);
+        int trgSeq = orgSeq + (int)dateDiff;
+        log.debug("DATE DIFF CALC: {}, {}, {}", orgSeq, dateDiff, trgSeq);
 
-        LocalDate firstDayOfYear = LocalDate.of(year, 1, 1);
-        int daysDiff = firstDayOfYear.until(resultDate).getDays();
-        log.debug("결과 일자 차이: {}", daysDiff);
-
-        int result = orgSeq + daysDiff;
         log.debug("{}일의 일련번호가 {}입니까? 그럼 {}일의 일련번호는 {}으로 예상됩니다.",
-                orgDateStr, orgSeq, trgDateStr, result);
-        System.out.println(orgSeq + "번째 날은 " + year + "년의 " + (daysDiff + 1) + "번째 날입니다.");
+                orgDateStr, orgSeq, trgDateStr, trgSeq);
 
-        return result;
+        return trgSeq;
 
     }
 
     @Test
     public void test1() {
         String dateOrg = "20230702";
-        String dateTarget = "20230702";
+        String dateTarget = "20230715";
         String currSeq = "296";
         int result = calc(dateOrg, dateTarget, currSeq);
         log.debug("결과 반환값: {}", result);
-
     }
 
 }
